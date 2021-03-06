7d67a4d92b6b459e0dd2cf07ec13958e8501a622
Osama Ahmed <56282088+thecode3@users.noreply.github.com>
2021-03-06 14:23:29 +0200

    Update Algorithm-sha256-Miner-AVR-Board.py

--- Algorithm-sha256-Miner-AVR-Board.py
+++ Algorithm-sha256-Miner-AVR-Board.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/env python3
+##########################################
+# Algorithm-sha256-Miner-AVR-Board (v0.0.1)
+# https://github.com/thecode3/Algorithm-sha256-Miner-AVR-Board
+# Distributed BSD 3-Clause LicenseSourcegraph version: 89375_2021-03-05_d646a50
+# Office Trade And Talk Community 2021.
+##########################################
